d80cd53a3e9d536ffac98b65ce39cdde66502c0e
Dave Lasley <dave@laslabs.com>
2017-09-04 10:27:23 -0700

    [ADD] Template Files * Codeclimate * Coveragerc * Gitignore * Travis * License (MIT) * setup

---
+++ README.rst
@@ -0,0 +1,61 @@
+|License MIT| | |Build Status| | |Coveralls Status| | |Codecov Status| | |Code Climate|
+
+============
+Python Five9
+============
+
+This library allows you to interact with Five9 using Python.
+
+Installation
+============
+
+* Install Python requirements ``pip install -r ./requirements``
+
+Setup
+=====
+
+* 
+
+Usage
+=====
+
+* `Read The API Documentation <https://laslabs.github.io/python-five9>`_
+
+Known Issues / Road Map
+=======================
+
+-  Installation, setup, usage - in ReadMe
+
+Credits
+=======
+
+Images
+------
+
+* LasLabs: `Icon <https://repo.laslabs.com/projects/TEM/repos/odoo-module_template/browse/module_name/static/description/icon.svg?raw>`_.
+
+Contributors
+------------
+
+* Dave Lasley <dave@laslabs.com>
+
+Maintainer
+----------
+
+.. image:: https://laslabs.com/logo.png
+   :alt: LasLabs Inc.
+   :target: https://laslabs.com
+
+This module is maintained by LasLabs Inc.
+
+.. |Build Status| image:: https://api.travis-ci.org/LasLabs/python-five9.svg?branch=master
+   :target: https://travis-ci.org/LasLabs/python-five9
+.. |Coveralls Status| image:: https://coveralls.io/repos/LasLabs/python-five9/badge.svg?branch=master
+   :target: https://coveralls.io/r/LasLabs/python-five9?branch=master
+.. |Codecov Status| image:: https://codecov.io/gh/LasLabs/python-five9/branch/master/graph/badge.svg
+   :target: https://codecov.io/gh/LasLabs/python-five9
+.. |Code Climate| image:: https://codeclimate.com/github/LasLabs/python-five9/badges/gpa.svg
+   :target: https://codeclimate.com/github/LasLabs/python-five9
+.. |License MIT| image:: https://img.shields.io/badge/license-MIT-blue.svg
+   :target: https://opensource.org/licenses/MIT
+   :alt: License: MIT
---
+++ setup.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-TODAY LasLabs Inc.
+# License MIT (https://opensource.org/licenses/MIT).
+
+from setuptools import Command, setup
+from setuptools import find_packages
+from unittest import TestLoader, TextTestRunner
+
+from os import environ, path
+
+
+PROJECT = 'python-five9'
+SHORT_DESC = 'This library allows for you to integrate with Five9 Cloud ' \
+             'Contact Center using Python.'
+README_FILE = 'README.rst'
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+version = environ.get('RELEASE') or environ.get('VERSION') or '0.0.0'
+
+if environ.get('TRAVIS_BUILD_NUMBER'):
+    version += 'b%s' % environ.get('TRAVIS_BUILD_NUMBER')
+
+
+setup_vals = {
+    'name': PROJECT,
+    'author': 'LasLabs Inc.',
+    'author_email': 'support@laslabs.com',
+    'description': SHORT_DESC,
+    'url': 'https://laslabs.github.io/python-five9',
+    'download_url': 'https://github.com/LasLabs/python-five9',
+    'license': 'MIT',
+    'classifiers': CLASSIFIERS,
+    'version': version,
+}
+
+
+if path.exists(README_FILE):
+    with open(README_FILE) as fh:
+        setup_vals['long_description'] = fh.read()
+
+
+install_requires = []
+if path.exists('requirements.txt'):
+    with open('requirements.txt') as fh:
+        install_requires = fh.read().splitlines()
+
+
+class FailTestException(Exception):
+    """ It provides a failing build """
+    pass
+
+
+class Tests(Command):
+    """ Run test & coverage, save reports as XML """
+
+    user_options = []  # < For Command API compatibility
+
+    def initialize_options(self, ):
+        pass
+
+    def finalize_options(self, ):
+        pass
+
+    def run(self, ):
+        loader = TestLoader()
+        tests = loader.discover('.', 'test_*.py')
+        t = TextTestRunner(verbosity=1)
+        res = t.run(tests)
+        if not res.wasSuccessful():
+            raise FailTestException()
+
+
+if __name__ == "__main__":
+    setup(
+        packages=find_packages(exclude=('tests')),
+        cmdclass={'test': Tests},
+        tests_require=[
+            'mock',
+        ],
+        install_requires=install_requires,
+        **setup_vals
+    )
